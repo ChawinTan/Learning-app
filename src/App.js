d76fe175fb56fa926c591ca85073c974122336ea
ChawinTan <chawin15@gmail.com>
2020-09-05 13:33:18 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyle = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },  
+  appBar: {
+    alignItems: 'center',
+    marginBottom: '2rem'
+  }
+}));
 
 function App() {
+  const classes = useStyle();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+     <AppBar position="static" className={classes.appBar} >
+       <Toolbar>
+         <Typography variant="h5">
+          Learning app
+         </Typography>
+       </Toolbar>
+     </AppBar>
+      <Typography align="center" variant="body1">
+         A simple starter app to hack around with new concepts and technologies for learning purposes.
+      </Typography>
+
     </div>
   );
 }
